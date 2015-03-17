522671a0ea0624b989fa21c9ee20b3dd3c820f27
Stephen Collins <rr154459@GA8995AC511228.local>
2015-03-16 20:23:11 -0400

    First attempt at task definition

--- tasks/jsontl.js
+++ tasks/jsontl.js
@@ -21,16 +21,57 @@ module.exports = function(grunt) {
 	}
 
 
-	grunt.registerMultiTask('jsontl', 'Perform jsontl transforms.', function() {
+	grunt.registerMultiTask('jsontl', 'Performing jsontl transformations', function() {
+
+		var result,
+				data,
+				transform;
+
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-			prettyPrint: grunt.option('prettyPrint') === true
+			prettyPrint: grunt.option('prettyPrint')
     });
 
     // Transform specified files.
-    this.filesSrc.forEach(function(filepath) {
-      transform(filepath, options);
-    });
+		this.files.forEach(function (f) {
+      var src = f.src.filter(function (filepath) {
+        // Warn on and remove invalid source files
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file ' + chalk.cyan(filepath) + ' not found.');
+          return false;
+        } else {
+          return true;
+        }
+      });
+
+      if (src.length === 0) {
+        grunt.log.warn('Destination ' + chalk.cyan(f.dest) + ' not written because src files were empty.');
+        return;
+      }
+
+			try {
+				data = grunt.file.readJSON(src);
+				transform = grunt.file.readJSON(this.transform);
+        result = jsontl.transform(data, transform);
+      } catch (e) {
+        console.log(e);
+        var err = new Error('Transform failed.');
+        if (e.message) {
+          err.message += '\n' + e.message + '. \n';
+          if (e.line) {
+            err.message += 'Line ' + e.line + ' in ' + src + '\n';
+          }
+        }
+        err.origError = e;
+        grunt.log.warn('Transformation of source ' + chalk.cyan(src) + ' failed.');
+        grunt.fail.warn(err);
+      }
+
+			grunt.file.write(f.dest, result);
+
+			grunt.verbose.writeln('File ' + chalk.cyan(f.dest) + ' created');
+      createdFiles++;
+		});
     grunt.log.ok(this.filesSrc.length  + ' ' + grunt.util.pluralize(this.filesSrc.length, 'path/paths') + ' cleaned.');
   });
 };
