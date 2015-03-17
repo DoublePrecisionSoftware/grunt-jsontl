fddcc37eb5f5c5dc331566cfe132f9b3be6ef629
Stephen Collins <rr154459@GA8995AC511228.local>
2015-03-16 22:18:34 -0400

    fixed configurations

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,58 @@
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
+	// Project configuration.
+	grunt.initConfig({
 
-    // Configuration to be run (and then tested).
-    jsontl: {
-      src: ['test/tmp/data_old.json'],
-      dest: ['test/tmp/data_new.json'],
-      transform: 'test/tmp/transform.jsontl',
-			options: {
-				prettyPrint: true
+		// Configuration to be run (and then tested).
+		jsontl: {
+			test: {
+				files: [{
+					expand: true,
+					src: ['data.old.json'],
+					dest: 'test/tmp',
+					cwd: 'test/tmp',
+					ext: '.new.json'
+				}],
+				transform: 'test/tmp/transform.jsontl',
+				options: {
+					prettyPrint: true
+				}
 			}
-    },
+		},
 
-    // Unit tests.
-    mochaTest: {
-      src: ['test/test.js'],
-    },
+		// Unit tests.
+		mochaTest: {
+			src: ['test/test.js'],
+			options: {
+				bail: true
+			}
+		},
 
 		clean: {
 			src: ['test/tmp/']
 		}
-  });
+	});
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-internal');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-mocha-tests');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-internal');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-mocha-test');
 
-  // Setup a test helper to create some folders to clean.
-  grunt.registerTask('copy', 'Copy fixtures to a temp location.', function() {
-    grunt.file.copy('test/fixtures/data.json', 'tmp/data_old.json');
-    grunt.file.copy('test/fixtures/transform.jsontl', 'tmp/transform.jsontl');
-  });
+	// Setup a test helper to create some folders to clean.
+	grunt.registerTask('copy', 'Copy fixtures to a temp location.', function () {
+		grunt.file.copy('test/fixtures/data.json', 'test/tmp/data.old.json');
+		grunt.file.copy('test/fixtures/transform.jsontl', 'test/tmp/transform.jsontl');
+	});
 
-  // Whenever the 'test' task is run, first create some files to be transformed,
-  // then run this plugin's task(s), then test the result.
-  grunt.registerTask('test', ['copy', 'mochaTest', 'clean']);
+	// Whenever the 'test' task is run, first create some files to be transformed,
+	// then run this plugin's task(s), then test the result.
+	grunt.registerTask('test', ['copy', 'jsontl', 'mochaTest', 'clean']);
 
-  // By default, and run all tests.
-//  grunt.registerTask('default', ['test', 'build-contrib']);
-  grunt.registerTask('default', ['copy', 'mochaTest', 'clean']);
+	// By default, run tranform, but don't clean up.
+	grunt.registerTask('default', ['copy', 'jsontl', 'mochaTest']);
 };
